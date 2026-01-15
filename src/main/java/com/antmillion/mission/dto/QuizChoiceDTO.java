9bbc22135da160df75dc1e6cd3ecd15cbe966961
GiJeong <rlwjd0025@gmail.com>
2026-01-15 10:48:31 +0900

    Refactor: DTO에서 @setter 일괄 제거

--- src/main/java/com/antmillion/mission/dto/QuizChoiceDTO.java
+++ src/main/java/com/antmillion/mission/dto/QuizChoiceDTO.java
@@ -3,10 +3,9 @@ package com.antmillion.mission.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
-@Getter@Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
--- src/main/java/com/antmillion/stock/dto/InterestDTO.java
+++ src/main/java/com/antmillion/stock/dto/InterestDTO.java
@@ -5,10 +5,9 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
-@Getter@Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
