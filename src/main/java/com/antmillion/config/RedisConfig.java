eebf0ec71d6a0d992e36c163433015fbf0287f5f
GAEUN JEONG <rkdms357@naver.com>
2026-01-15 14:32:30 +0900

    Fix: RedisConfig에 db.properties 로딩 추가

--- src/main/java/com/antmillion/config/RedisConfig.java
+++ src/main/java/com/antmillion/config/RedisConfig.java
@@ -5,6 +5,7 @@ import io.lettuce.core.protocol.ProtocolVersion;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
@@ -12,10 +13,12 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 
 @Configuration
+@PropertySource("classpath:db.properties")
 public class RedisConfig {
 
     @Value("${redis.host}")
