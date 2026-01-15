21eb7dbca10e7c42314eef16982bd95a37ec84e3
GAEUN JEONG <rkdms357@naver.com>
2026-01-15 22:28:33 +0900

    Feat: List<QuizChoiceDTO> choices 필드 추가

--- src/main/java/com/antmillion/mission/dto/QuizQuestionDTO.java
+++ src/main/java/com/antmillion/mission/dto/QuizQuestionDTO.java
@@ -1,6 +1,7 @@
 package com.antmillion.mission.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,4 +22,7 @@ public class QuizQuestionDTO {
     private String explanation;
     private Integer point;
     private LocalDateTime quizDate;
+
+    // 객관식일 경우 보기들을 담아서 화면에 보내줄 리스트
+    private List<QuizChoiceDTO> choices;
 }
