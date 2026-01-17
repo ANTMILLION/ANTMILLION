53f83be22db9c8b981b3c04398a15fbe6cacd38e
GAEUN JEONG <rkdms357@naver.com>
2026-01-17 20:16:23 +0900

    Feat: 미션(퀴즈) 오늘의 퀴즈 조회 및 서비스 로직 구현

--- src/main/java/com/antmillion/mission/controller/MissionController.java
+++ src/main/java/com/antmillion/mission/controller/MissionController.java
@@ -1,15 +1,30 @@
 package com.antmillion.mission.controller;
 
+import com.antmillion.mission.dto.QuizQuestionResponseDTO;
+import com.antmillion.mission.service.MissionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/mission")
+@RequiredArgsConstructor
 public class MissionController {
+
+    private final MissionService missionService;
 
     @GetMapping
     public String missionPage() {
         return "mission/mission";
+    }
+
+    @GetMapping("/daily")
+    @ResponseBody
+    public List<QuizQuestionResponseDTO> getDailyQuizData() {
+        return missionService.getDailyQuiz();
     }
 }
