a88a5e02858349b10e2fdbf0b48462f97a606f6e
taewon0 <ktw495@naver.com>
2026-01-17 15:40:34 +0900

    Refactor: Service를 하나로 통합

--- src/main/java/com/antmillion/kis/service/KisApiService.java
+++ src/main/java/com/antmillion/kis/service/KisApiService.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -39,14 +40,19 @@ public class KisApiService {
 
         // Redis 저장 (TTL 자동 설정)
         kisAccessTokenRedisRepository.save(
-                response.getAccessToken(),
-                response.getExpiresIn()
+                response.getAccess_token(),
+                response.getExpires_in()
         );
 
-        return response.getAccessToken();
+        return response.getAccess_token();
     }
 
-    public KisAccessTokenResponse issueAccessToken() {
+    /**
+     * 신규 접근 토큰 발급
+     * 24시간 유효, 6시간마다 재발급 가능, api 호출 시 1분간 재호출 제한
+     * @return 접근 토큰, 유효시간
+     */
+    private KisAccessTokenResponse issueAccessToken() {
 
         String url = config.baseUrl + "/oauth2/tokenP";
         KisAccessTokenRequest body = new KisAccessTokenRequest();
@@ -61,8 +67,31 @@ public class KisApiService {
         return response.getBody();
     }
 
-    public List<ChartStockPriceRequest> getHistoricalStockPrices() {
+    /**
+     * 국내주식 기간별 시세 조회
+     * @param request  조회 조건 (종목코드, 기간, 날짜 등)
+     * @return 캔들 차트 데이터 리스트
+     */
+    public List<ChartStockPriceRequest> getPeriodStockPrices(ChartStockPriceRequest request) {
+        String token = getKisAccessToken();
+        HttpHeaders headers = createPeriodApiHeader(token);
+        String url = buildPeriodApiUrl();
         return null;
+    }
+
+    private String buildPeriodApiUrl() {
+    }
+
+    private HttpHeaders createPeriodApiHeader(String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("content-type", "application/json; charset=utf-8");
+        headers.setBearerAuth(token);
+        headers.set("appkey", config.appKey);
+        headers.set("appsecret", config.appSecret);
+        headers.set("tr_id", "FHKST03010100");  // 국내주식 기간별시세 TR_ID
+        headers.set("custtype", "P");
+        return headers;
+
     }
 
 }
