491e30455f1dd5d57441ad6cec74abc2d8f5baaa
taewon0 <ktw495@naver.com>
2026-01-20 15:26:15 +0900

    Feat: 관심종목 Mapper 인터페이스 작성

---
+++ src/main/java/com/antmillion/stock/mapper/InterestMapper.java
@@ -0,0 +1,22 @@
+package com.antmillion.stock.mapper;
+
+import com.antmillion.stock.dto.InterestDTO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface InterestMapper {
+
+    //관심종목 추가
+    int insertInterest(InterestDTO interest);
+
+    //관심종목 삭제
+    int deleteInterest(@Param("stockCode") String stockCode, @Param("accountId") String accountId);
+
+    //특정 계정의 관심종목 조회
+    List<String> selectInterestStockCodesByAccountId(@Param("accountId") String accountId);
+
+    // 특정 종목이 관심종목인지 확인
+    int countInterest(@Param("stockCode") String stockCode, @Param("accountId") Long accountId);
+
+}
