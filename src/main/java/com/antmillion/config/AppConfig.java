c57f8279835fc3e0167aa494229af326bd57eb18
ahyeonkong <kimah0403@gmail.com>
2026-01-20 10:45:23 +0900

    Refactor: 스케줄러 관련 로직 제거

--- src/main/java/com/antmillion/config/AppConfig.java
+++ src/main/java/com/antmillion/config/AppConfig.java
@@ -3,10 +3,8 @@ package com.antmillion.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableScheduling
 public class AppConfig {
 
     @Bean
