6d9108e8957282c78debc3a9c1e480c3cd9b35dd
taewon0 <ktw495@naver.com>
2026-01-23 17:21:23 +0900

    Feat: 분기로 호가 받는 로직 구현

--- src/main/java/com/antmillion/kis/handler/KisWebSocketHandler.java
+++ src/main/java/com/antmillion/kis/handler/KisWebSocketHandler.java
@@ -45,7 +45,6 @@ public class KisWebSocketHandler extends TextWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) {
     	// 역할: 데이터 수신 응답 및 처리 (OnMessage)
         String payload = message.getPayload();
-        
         // 1. PINGPONG 처리
         if (payload.contains("PINGPONG")) {
         	manager.updateLastHeartbeatTime(); // 수신 시간 갱신
@@ -70,7 +69,7 @@ public class KisWebSocketHandler extends TextWebSocketHandler {
         // 3. 실시간 실무 데이터 처리 (문자열 파싱)
         try {
             String[] parts = payload.split("\\|");
-            if ("H0STCNT0".equals(parts[1])) {
+            if ("H0UNCNT0".equals(parts[1])) {
             	String[] data = parts[3].split("\\^");
             	// [0], [2], [4], [5], [22]
                 Map<String, String> tradeData = new HashMap<>();
@@ -82,8 +81,38 @@ public class KisWebSocketHandler extends TextWebSocketHandler {
                 
                 String stockCode = data[0];
                 // STOMP 전송
-                messagingTemplate.convertAndSend("/topic/kis-trade/" + stockCode, tradeData); // 실시간 체결가
+                messagingTemplate.convertAndSend("/topic/kis-trade/present" + stockCode, tradeData); // 실시간 체결가
                 System.out.println("체결가 수신: " + tradeData);
+            } else if ("H0UNASP0".equals(parts[1])) {
+                String[] data = parts[3].split("\\^");
+                Map<String, String> askBidData = new HashMap<>();
+                askBidData.put("mkscShrnIscd", data[0]); // 종목코드
+                askBidData.put("askP1", data[3]);
+                askBidData.put("askP2", data[4]);
+                askBidData.put("askP3", data[5]);
+                askBidData.put("askP4", data[6]);
+                askBidData.put("askP5", data[7]);
+                askBidData.put("bidP1", data[13]);
+                askBidData.put("bidP2", data[14]);
+                askBidData.put("bidP3", data[15]);
+                askBidData.put("bidP4", data[16]);
+                askBidData.put("bidP5", data[17]);
+                askBidData.put("askPrsqn1", data[23]);
+                askBidData.put("askPrsqn2", data[24]);
+                askBidData.put("askPrsqn3", data[25]);
+                askBidData.put("askPrsqn4", data[26]);
+                askBidData.put("askPrsqn5", data[27]);
+                askBidData.put("bidPrsqn1", data[33]);
+                askBidData.put("bidPrsqn2", data[34]);
+                askBidData.put("bidPrsqn3", data[35]);
+                askBidData.put("bidPrsqn4", data[36]);
+                askBidData.put("bidPrsqn5", data[37]);
+
+                String stockCode = data[0];
+                messagingTemplate.convertAndSend("/topic/kis-trade/ask-bid" + stockCode, askBidData);
+                System.out.println("호가 수신: " +  askBidData);
+            } else {
+                System.out.println("현재가/호가 아님");
             }
         } catch (Exception e) {
             System.err.println("데이터 처리 오류: " + e.getMessage());
