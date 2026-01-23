d7259296a7d7cb5fa596086adb6ba574754ad11c
ahyeonkong <kimah0403@gmail.com>
2026-01-23 11:42:17 +0900

    Feat: 웹소켓과 STOMP 연결 테스트용 화면 구현

--- src/main/webapp/WEB-INF/views/kis/KisWebSocketTransactionPrice.jsp
+++
@@ -1,9 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.5.1/sockjs.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/stomp.js/2.3.3/stomp.min.js"></script>
-<div id="stock-panel">
-    <h3>실시간 체결 정보 (<span id="stock-code">005930</span>)</h3>
-    <p>현재가: <span id="price" style="font-weight:bold;">-</span></p>
-    <p>전일대비: <span id="vrss">-</span></p>
-    <p>대비율: <span id="ctrt">-</span>%</p>
-    <p>매수비율: <span id="rate">-</span>%</p>
-</div>
--- src/main/webapp/resources/js/kis/kisWebSocketTransactionPrice.js
+++ src/main/webapp/resources/js/kis/kisWebSocketTransactionPrice.js
@@ -2,7 +2,10 @@ var stompClient = null;
 
 function connect() {
     // 1. Spring Config에서 설정한 Endpoint 주소 (예: /ws-stomp)
-    var socket = new SockJS('/ws-stomp'); 
+    var url = contextPath + '/ws-stomp';
+    console.log("연결 시도 주소: " + url); // 브라우저 콘솔에서 주소 맞는지 확인용
+    
+    var socket = new SockJS(url);
     stompClient = Stomp.over(socket);
 
     stompClient.connect({}, function (frame) {
@@ -10,16 +13,15 @@ function connect() {
 
         // 2. 백엔드에서 쏘는 Topic 주소를 구독
         stompClient.subscribe('/topic/kis-trade', function (response) {
+
             // 전달받은 데이터를 JSON으로 파싱
             var tradeData = JSON.parse(response.body);
+            
+                console.log("수신 데이터:", tradeData);
             
             // 화면 갱신 함수 호출
             updateUI(tradeData);
         });
-    }, function (error) {
-        console.error('STOMP error: ' + error);
-        // 연결 끊기면 5초 후 재시도
-        setTimeout(connect, 5000);
     });
 }
 
@@ -30,7 +32,7 @@ function updateUI(data) {
     document.getElementById('ctrt').innerText = data.prdyCtrt;
     document.getElementById('rate').innerText = data.shnuRate;
 
-    // 가격 등락에 따른 색상 변경 로직 (꿀팁)
+    // 가격 등락에 따른 색상 변경 로직
     var priceTag = document.getElementById('price');
     if (parseFloat(data.prdyCtrt) > 0) {
         priceTag.style.color = 'red';
@@ -42,6 +44,7 @@ function updateUI(data) {
 }
 
 // 페이지 로드 시 연결 시작
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM 로드 완료, 연결 시작!");
     connect();
-};
+});
