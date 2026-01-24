f86f3484b505bc22f05ca27b90067f6ea76a5e6a
GAEUN JEONG <rkdms357@naver.com>
2026-01-24 19:28:19 +0900

    Feat: 퀴즈 정답 시 설명 제공 기능 추가
    - QuizResultDTO 생성 (answer, explanation, point)
    - QuizSubmissionResponseDTO 생성 (isCorrect, point, message)
    - 정답 시 explanation 반환, 오답 시 안내 메시지 반환
    - selectQuizResultByQuizId 쿼리로 기존 정답, 퀴즈 포인트 통합 조회하여 DB부하 낮춤
    - checkAnswer, checkAndLogAnswer 반환타입 QuizSubmissionResponseDTO로 변경

--- src/main/java/com/antmillion/mission/service/MissionService.java
+++ src/main/java/com/antmillion/mission/service/MissionService.java
@@ -2,13 +2,14 @@ package com.antmillion.mission.service;
 
 import com.antmillion.mission.dto.QuizQuestionResponseDTO;
 import com.antmillion.mission.dto.QuizSubmissionRequestDTO;
+import com.antmillion.mission.dto.QuizSubmissionResponseDTO;
 import com.antmillion.user.dto.UserRankResponseDTO;
 
 import java.util.List;
 
 public interface MissionService {
     List<QuizQuestionResponseDTO> getDailyQuiz();
-    boolean checkAndLogAnswer(QuizSubmissionRequestDTO requestDTO);
+    QuizSubmissionResponseDTO checkAndLogAnswer(QuizSubmissionRequestDTO requestDTO);
     UserRankResponseDTO getUserMissionStatus(Long userId);
     Long getCurrentUserId();
     boolean isTodayMissionCompleted(Long userId);
